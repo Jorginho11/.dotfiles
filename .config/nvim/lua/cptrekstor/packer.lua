f3efca529b1a12111b8d3e819140a3eea7533a33
Jorginho11 <jorge_v_oliveira@hotmail.com>
2024-08-15 14:53:22 -0500

    fixing packer

--- .config/nvim/lua/cptrekstor/packer.lua
+++ .config/nvim/lua/cptrekstor/packer.lua
@@ -1,6 +1,17 @@
 
 -- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
@@ -71,4 +82,7 @@ return require('packer').startup(function(use)
   use "fatih/vim-go"
   -- Ruby config
   use "vim-ruby/vim-ruby"
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
